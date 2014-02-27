d35296b4536b1037dc39dd3f0457cc97c13a0b61
Connor Cook <cookcon1@umbc.edu>
2014-02-26 19:47:20 -0500

    Added custom splash page for our team and ensured routes and views can be modified without compromising integrity of the website.

---
+++ digidiet/app/views/layouts/standard.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Welcome to DIGIDIET.com!</title>
+  <style>
+		@import url(//fonts.googleapis.com/css?family=Lato:700);
+		
+		body {
+			margin:0;
+			font-family:'Lato', sans-serif;
+			text-align:center;
+			color: #000000;
+			left: 50%;
+			top: 50%;
+		}
+
+		.container {
+			border:2px solid #FF0000;
+			border-radius:25px;
+			margin-left: 50px;
+			margin-top: 50px;
+			margin-bot: 50px;
+			margin-right: 50px;
+			
+		 }
+
+		a, a:visited {
+			text-decoration:none;
+		}
+
+		h1 {
+			font-size: 32px;
+			margin: 16px 0 0 0;
+		}
+	</style>
+</head>
+<body>
+	    
+        <div class="container">
+            @yield('content')
+        </div>
+</body>
+</html>
---
+++ digidiet/app/views/splash.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.standard')
+
+@section('content')
+     <h2>DIGIDIET</h2>
+     <body>
+     Food of the future.
+     Food of the world.
+     </body>
+@stop
