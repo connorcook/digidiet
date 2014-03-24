15aeeba3b385278b3b9e79b6adad79e590273936
connorcook <cookcon1@umbc.edu>
2014-03-24 00:14:00 -0400

    Installed KickStart, removed METRO-UI, redid standard blade layout, set up stage for redesign of main blade templates. New layouts are smartphone compatible and support nearly all browsers.

--- digidiet/app/views/layouts/standard.blade.php
+++ digidiet/app/views/layouts/standard.blade.php
@@ -3,109 +3,48 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>DIGIDIET!</title>
-  <!-- <link rel="stylesheet" href="/css/metro-bootstrap.css"> -->
-  <!-- <script src="/js/jquery.min.js"></script>
-  // <script src="/js/jquery.widget.min.js"></script>
-  // <script src="/js/metro.min.js"></script> -->
-  {{ HTML::style('/css/metro-bootstrap.css'); }}
-  {{ HTML::style('/css/metro-bootstrap-responsive.css'); }}
-  {{ HTML::style('/css/iconFont.css'); }}
-  {{ HTML::script('/js/jquery.min.js'); }}
+  <title>Working on a Title</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <meta name="description" content="" />
+
+  
+  {{ HTML::script('/js/jquery.min.js'); }} <!-- JQUERY   -->
   {{ HTML::script('/js/jquery.widget.min.js'); }}
-  {{ HTML::script('/js/metro.min.js'); }}
-  {{ HTML::script('/js/metro-button-set.js'); }}
+  {{ HTML::script('/js/kickstart.js'); }} <!-- KICKSTART -->
+  {{ HTML::style('/css/kickstart.css'); }} <!-- KICKSTART -->
+  {{ HTML::style('/style.css'); }} <!-- KICKSTART -->
 
-
-  <style>
-		@import url(//fonts.googleapis.com/css?family=Lato:700);
-		
-		body {
-			/*margin:0;
-			font-family:'Lato', sans-serif;*/
-			font-size: 20px;
-			font-color: #FF7400;
-			text-align:center;
-			color: #000000;
-			
-		    background-image: url({{ URL::asset('/images/food.png'); }})
-
-		}
-
-
-		.container {
-			
-			border:3px solid #216268;
-			border-radius:15px;
-			width: 60%;
-			margin: 20% auto;
-			padding: 10px;
-
-		    background-color:#009999;
-
-		       opacity:0.7;
-			
-		 }
-		 h2{
-			font-size:  64px;
-			font-color: #A64B00;
-			margin-bottom: 5px;
-			margin-top: 5px;
-		}
-
-		
-	</style>
 </head>
-<body class="metro">
+<body>
+<style>
+  background-image: url({{ URL::asset('/images/food.png'); }});
 
-<nav class="navigation-bar light">
-	<nav class="navigation-bar-content">
-		<div class="element">
-			<a class="dropdown-toggle" href="#">NAVIGATE</a>
-			<ul class="dropdown-menu" data-role="dropdown">
-				<li><a href="#">Home</a></li>
-				<li><a href="#">Profile</a></li>
-				<li><a href="#">Settings</a></li>
-			</ul>
-		</div>
+</style>
 
-		<span class="element-divider"></span>
-		<a class="element brand" href="#"><span class="icon-spin"></span></a>
-        <a class="element brand" href="#"><span class="icon-printer"></span></a>
-        <span class="element-divider"></span>
-        
-
-		<div class="element input-element">
-            <form>
-                <div class="input-control text">
-                    <input type="text" placeholder="Search...">
-                    <button class="btn-search"></button>
-                </div>
-            </form>
-        </div>
-
-        <span class="element-divider"></span>
-
-        <div class="element place-right">
-            <a class="dropdown-toggle" href="#">
-                <span class="icon-cog"></span>
-            </a>
-            <ul class="dropdown-menu place-right" data-role="dropdown">
-                <li><a href="#">Products</a></li>
-                <li><a href="#">Download</a></li>
-                <li><a href="#">Support</a></li>
-                <li><a href="#">Buy Now</a></li>
-            </ul>
-        </div>
-
-        <span class="element-divider place-right"></span>
-        <a class="element place-right" href="#"><span class="icon-locked-2"></span></a>
-        <span class="element-divider place-right"></span>
-        <a class="element place-right" href="#"><span class="icon-user"></span></a>
-        <button class="element image-button image-left place-right">
+<!-- Menu Horizontal -->
+<ul class="menu">
+<li class="current"><a href="">Item 1</a></li>
+<li><a href="">Item 2</a></li>
+<li><a href=""><span class="icon" data-icon="R"></span>Item 3</a>
+  <ul>
+  <li><a href=""><span class="icon" data-icon="G"></span>Sub Item</a></li>
+  <li><a href=""><span class="icon" data-icon="A"></span>Sub Item</a>
+    <ul>
+    <li><a href=""><span class="icon" data-icon="Z"></span>Sub Item</a></li>
+    <li><a href=""><span class="icon" data-icon="k"></span>Sub Item</a></li>
+    <li><a href=""><span class="icon" data-icon="J"></span>Sub Item</a></li>
+    <li><a href=""><span class="icon" data-icon="="></span>Sub Item</a></li>
+    </ul>
+  </li>
+  <li class="divider"><a href=""><span class="icon" data-icon="T"></span>li.divider</a></li>
+  </ul>
+</li>
+<li><a href="">Item 4</a></li>
+</ul>
 
 
-        @if ( Auth::guest())
+        <!-- @if ( Auth::guest())
           
             <a class="btn" href="{{URL::to('login')}}"> Login </a>
             <span class="element-divider"></span>
@@ -114,15 +53,75 @@
         @else
             {{ HTML::link('admin', Auth::user()->username)}}
             {{ HTML::link('logout', 'Logout')}}
-        @endif    
-        </button>
-	</nav>
-</nav>
+        @endif     -->
 
-        <div class="container">
-        @yield('content')
-        <!-/container->
-        
+<div class="grid">
+<div class="col_12">
+  <!-- MAIN CONTENT SECTION -->
+  <div class="col_9">
+    @yield('content')
+  </div>
+  
+  <div class="col_3">
+  <h5>Now Serving</h5>
+  <ul class="icons">
+  <li><i class="icon-ok"></i> Drinks</li>
+  <li><i class="icon-ok"></i> Soups</li>
+  <li><i class="icon-ok"></i> Entrees</li>
+  <li><i class="icon-ok"></i> Festive Adult Delights</li>
+  </ul>
+  
+  <h5>Share</h5>
+  <i class="icon-twitter-sign icon-4x"></i> 
+  <i class="icon-facebook-sign icon-4x"></i>
+  <i class="icon-linkedin-sign icon-4x"></i>
+  <i class="icon-github-sign icon-4x"></i>
+  
+  <span class="icon social x-large darkgray" data-icon="1"></span>
+  <span class="icon social x-large black" data-icon="w"></span>
+  <span class="icon x-large pink" data-icon="*"></span>
+  <span class="icon social x-large green" data-icon="v"></span>
+  
+  <h5>RSS Feed</h5>
+  <a class="button orange small" href="#"><i class="icon-rss"></i> RSS</a>
+  </div>
+  
+  <hr />
+  
+  <div class="col_3">
+  <h4>Drinks</h4>
+  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore 
+  magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis</p>
+  </div>
+  
+  <div class="col_3">
+  <h4>Soups</h4>
+  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore 
+  magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis</p>
+  </div>
+  
+  <div class="col_3">
+  <h4>Entrees</h4>
+  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore 
+  magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis</p>
+  </div>
+  
+  <div class="col_3">
+  <h4>Festive Adult Delights</h4>
+  <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore 
+  magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis</p>
+  </div>
+</div>
+
+</div><!-- END GRID -->
+
+<!-- FOOTER -->
+<div class="clear"></div>
+<div id="footer">
+&copy; Copyright 2014 All Rights Reserved.
+</div>
+
+</body></html> 
 
 </body>
 </html>
--- digidiet/app/views/splash.blade.php
+++ digidiet/app/views/splash.blade.php
@@ -1,22 +1,10 @@
 @extends('layouts.standard')
 
 @section('content')
-     <h2>DigiDiet</h2>
-     <hr style="border-color: #216268; width: 75%"/>
-     <body>
-     Just another reason to love cooking.
-     Coming soon.
-     <hr style="border-color: #216268; width: 75%"/>
-     Sign up for updates: {{ Form::email($name = "updateEmail", $value = null, $attributes = array()); }}
-     </body>
-     </div></div>
-
-     @foreach($recipes as $recipe)
-     <div="container">
-     	<body>
+     <h3>Working Title Recipes</h3>
+     <p>Just another reason to love cooking. Coming soon.</p>
+     <!-- @foreach($recipes as $recipe)
      	<h2>{{ 	$recipe->recipe_name }} </h2>
      	<p>	{{ 	$recipe->recipe_body }} </p>
-     	</body>
-     </div>
-     @endforeach
+     @endforeach -->
 @stop
