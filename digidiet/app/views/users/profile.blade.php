4304e664260dc1f58d4c61b5c92b48f7275bcd7d
connorcook <cookcon1@umbc.edu>
2014-03-31 15:53:27 -0400

    Further completed UserController with working profile pages.

--- digidiet/app/views/users/profile.blade.php
+++ digidiet/app/views/users/profile.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.content')
 
 @section('content')
-	<!--
-	<h4> 	$user->username </h4>
-	<p>	 	$user->name </p>
-	-->
+	<h4>{{ isset($user) ? $user->username."'s Profile" : 'User not found.' }}</h4>
+	<h5>Real name: {{ isset($user) ? $user->name : '' }}</h5>
+	<hr class="alt1" />
+	<h5>About</h5>
+	<p> {{ isset($user) ? $user->about_me : 'The user with the specified ID was not found. Please contact an administrator.' }}</p>
 @endsection
