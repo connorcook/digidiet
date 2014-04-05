9da50fc0ba8ff8b8e07c4ab94eaf4f7dac3946b3
bmattox1 <bmattox1@umbc.edu>
2014-04-04 21:14:09 -0400

    Added more recipes to seed the database with. I mostly did this for myself for testing purposes, as the post recipe functionality does not exist yet. Run php artisan db:seed to reseed

--- digidiet/app/database/seeds/DatabaseSeeder.php
+++ digidiet/app/database/seeds/DatabaseSeeder.php
@@ -55,6 +55,91 @@ class RecipeTableSeeder extends Seeder {
                 'instructions' => "Take butter, melt, pour over dough, cook at 400 degrees Fahrenheit for 20 minutes.",
                 'ingredients' => "Butter, Dough, Oven, and a Smile"
         ));
+
+        Recipe::create(array(
+                'name' => "Mom's pea soup",
+                'author_id' => 1,
+                'instructions' => "Boil water. Throw in split peas, carrots, ham, potatoes, onion, garlic. Cook until vegetables are soft. Season with salt and pepper.",
+                'ingredients' => "Water, split peas, carrots, potatoes, ham, onion, garlic, salt",
+        ));
+        Recipe::create(array(
+                'name' => 'Homemade Chicken Pot Pie',
+                'author_id' => 1,
+                'instructions' => "",
+                'ingredients' => "",
+        ));
+        Recipe::create(array(
+                'name' => 'Sushi Alaska Roll',
+                'author_id' => 2,
+                'instructions' => "Roll up some salmon, cucumber, and avocado in rice and seaweed.",
+                'ingredients' => "Rice, seaweed, salmon, cucumber, avocado, sugar, vinegar",
+        ));
+        Recipe::create(array(
+                'name' => 'Coffee-marinated Pork Chops',
+                'author_id' => 2,
+                'instructions' => "Marinate pork chops in coffee marinade over night (12+ hours). Grill over flame til desired doneness.",
+                'ingredients' => "Coffee, worcestershire sauce, pork chops",
+        ));
+        Recipe::create(array(
+                'name' => 'Eggs Benedict',
+                'author_id' => 3,
+                'instructions' => "",
+                'ingredients' => "",
+        ));
+        Recipe::create(array(
+                'name' => 'Chocolate Chip Pancakes',
+                'author_id' => 3,
+                'instructions' => "",
+                'ingredients' => "",
+        ));
+        Recipe::create(array(
+                'name' => 'Bowl of Cereal',
+                'author_id' => 3,
+                'instructions' => "Pour cereal in bowl, then pour milk in bowl.",
+                'ingredients' => "Cereal, milk",
+        ));
+        Recipe::create(array(
+                'name' => 'Cheesy Macaroni and Cheese',
+                'author_id' => 4,
+                'instructions' => "Cook some noodles, then melt some cheese in them.",
+                'ingredients' => "Noodles, Cheese, salt, pepper",
+        ));
+        Recipe::create(array(
+                'name' => 'Creamy Macaroni and Cheese',
+                'author_id' => 4,
+                'instructions' => "Cook some noodles, then melt some cheese in them.",
+                'ingredients' => "Noodles, heavy cream, cheese",
+        ));
+        Recipe::create(array(
+                'name' => 'Old fashioned',
+                'author_id' => 4,
+                'instructions' => "",              
+                'ingredients' => "",
+        ));
+        Recipe::create(array(
+                'name' => 'Worcester Steak',
+                'author_id' => 5,
+                'instructions' => "Marinate steak overnight in secret worcestershire-style sauce. Grill over flame til desired doneness",
+                'ingredients' => "Worcestershire sauce, steak, cumin, salt, pepper,"
+        ));
+        Recipe::create(array(
+                'name' => "Grandma's Pecan Pie",
+                'author_id' => 6,
+                'instructions' => "A whole lot of candied pecans in a pie shell. Bake at 350 deg F for 40 minutes.",
+                'ingredients' => "Candied pecans, pie crust",
+        ));
+        Recipe::create(array(
+                'name' => 'The Best Philly Cheesesteak',
+                'author_id' => 7,
+                'instructions' => "",
+                'ingredients' => "",
+        ));
+        Recipe::create(array(
+                'name' => 'Cheesy Cheese Pizza',
+                'author_id' => 7,
+                'instructions' => "Roll dough, put on sauce, put on cheese, bake in 400 degree Fahrenheit oven for 20 minutes",
+                'ingredients' => "Water, flour, pizza sauce, mozzarella cheese",
+        ));
         
         for($i = 0; $i < 100; $i++){
         	$recipe = array(
