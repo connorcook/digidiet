2f34298150744721ac059df9831c016eab677af7
bmattox1 <bmattox1@umbc.edu>
2014-04-07 22:19:27 -0400

    Changed fieldname from 'name' to 'title'

--- digidiet/app/views/results.blade.php
+++ digidiet/app/views/results.blade.php
@@ -16,22 +16,8 @@
      <p>{{ Form::submit('Search', array('class' => 'btn-large')); }}</p>
      {{ Form::close(); }}
 
-<!--<form action="" method="get">
-	Search for recipes: <input type="text" name="search_query">
-	<input type="submit" value="Search">
-</form>
--->
-
-<!--
 <?php
-	if(isset($_POST['search_query'])) {
-		$search = $_POST['search_query'];
-		// TODO FIGURE OUT HOW TO REFRESH PAGE WITH NEW SEARCH RESULTS
-	}
-?>
--->
 
-<?php
 /* Function for opening a database connection */
 function openDBConn() {
     static $link;
@@ -40,6 +26,7 @@ function openDBConn() {
     }
     return $link; // returns the link
 }
+
 	$dblink = openDBConn();
 	if (!$dblink) {
 		echo( "<P>Unable to connect to the " .
@@ -68,31 +55,33 @@ function openDBConn() {
 		switch ($sortby_option) {
 			case "highest rating":
 //				echo "User wishes to sort by highest rating";
-				$result = mysqli_query($dblink, "SELECT * FROM recipes WHERE name LIKE'%" . mysqli_real_escape_string($dblink, $search) . "%' ORDER BY created_at DESC", MYSQLI_USE_RESULT);
+				$result = mysqli_query($dblink, "SELECT * FROM recipes WHERE title LIKE'%" . mysqli_real_escape_string($dblink, $search) . "%' ORDER BY created_at DESC", MYSQLI_USE_RESULT);
 				break;
 			case "lowest rating":
 //				echo "User wishes to sort by lowest rating.";
-				$result = mysqli_query($dblink, "SELECT * FROM recipes WHERE name LIKE'%" . mysqli_real_escape_string($dblink, $search) . "%' ORDER BY created_at DESC", MYSQLI_USE_RESULT);
+				$result = mysqli_query($dblink, "SELECT * FROM recipes WHERE title LIKE'%" . mysqli_real_escape_string($dblink, $search) . "%' ORDER BY created_at DESC", MYSQLI_USE_RESULT);
 				break;
-			case "more recent":
+			case "most recent":
 //				echo "User wishes to sort by more recent.";
-				$result = mysqli_query($dblink, "SELECT * FROM recipes WHERE name LIKE'%" . mysqli_real_escape_string($dblink, $search) . "%' ORDER BY created_at DESC", MYSQLI_USE_RESULT);
+				$result = mysqli_query($dblink, "SELECT * FROM recipes WHERE title LIKE'%" . mysqli_real_escape_string($dblink, $search) . "%' ORDER BY created_at DESC", MYSQLI_USE_RESULT);
 				break;
-			case "less recent":
+			case "least recent":
 //				echo "User wishes to sort by less recent.";
-				$result = mysqli_query($dblink, "SELECT * FROM recipes WHERE name LIKE'%" . mysqli_real_escape_string($dblink, $search) . "%' ORDER BY created_at", MYSQLI_USE_RESULT);
+				$result = mysqli_query($dblink, "SELECT * FROM recipes WHERE title LIKE'%" . mysqli_real_escape_string($dblink, $search) . "%' ORDER BY created_at", MYSQLI_USE_RESULT);
 				break;
 			default:
 //				echo "User wishes to sort by {$sortby_option}, but that is not a sort option.";
 		}
 	} else {
-		$result = mysqli_query($dblink, "SELECT * FROM recipes WHERE name LIKE'%" . mysqli_real_escape_string($dblink, $search) . "%'", MYSQLI_USE_RESULT);
+		$result = mysqli_query($dblink, "SELECT * FROM recipes WHERE title LIKE'%" . mysqli_real_escape_string($dblink, $search) . "%'", MYSQLI_USE_RESULT);
 	}
 	if (!$result) {
 		echo("<P>Error performing query: " .
     	mysqli_error($dblink) . "</P>");
 		exit();
 	}
+
+	print_r($_POST);		// TESTING PURPOSES ONLY
 
 	/* Create an array for storing the result set,
 	 * and create a table to hold the results for
@@ -112,7 +101,7 @@ function openDBConn() {
     {
     	$results[] = $row;
     	echo "<tr>";
-    	echo "<td>" . $row['name'] . "</td>";
+    	echo "<td>" . $row['title'] . "</td>";
     	//echo "<td>" . $row['rating'] . "</td>";
     	echo "</tr>";
     }
@@ -120,9 +109,6 @@ function openDBConn() {
 //  print_r($results);		// TESTING PURPOSES ONLY
     mysqli_free_result($result);	// Free result set
 
-   	/* TODO Call preg_match to match on regular expressions */
-?>
-<?php
 	$results_count = count($results);
 	echo "<br>{$results_count}";
 	if($results_count== 1) { echo ' result'; } else { echo ' results';} 
@@ -134,14 +120,11 @@ function openDBConn() {
     <select multiple name="sortby[]">
         <option value="highest rating" name="sort_option">Highest Rating</option>
         <option value="lowest rating" name="sort_option">Lowest Rating</option>
-        <option value="most comments" name="sort_option">Most Comments</option>
-        <option value="least comments" name="sort_option">Least Comments</option>
-        <option value="more recent" name="sort_option">More Recent</option>
-        <option value="less recent" name="sort_option">Less Recent</option>
+        <option value="most recent" name="sort_option">Most Recent</option>
+        <option value="least recent" name="sort_option">Least Recent</option>
     </select><br />
     <br />
     <input type="submit" value="Sort"/>
 </form>
-
 @stop
 @endsection
