a171aef5b2b8fb7efe5272b79c47c76c9825defb
murdock2 <murdock2@umbc.edu>
2014-04-14 03:32:16 -0400

    added screenshots and page/navigation descriptions to UI, Recipe views added to CIR with updates to views as documented

--- digidiet/app/views/recipes/directory.blade.php
+++ digidiet/app/views/recipes/directory.blade.php
@@ -7,11 +7,13 @@
 @section('content')
 	<h1> Recipes Directory </h1>
 	<div class="grid">
+
 	@foreach($recipes as $recipe)
 		<a href="/recipe/{{$recipe->id}}">
 		<h6>{{$recipe -> title}}</h6> </a>
 		<p>{{$recipe -> description}}</p>
 	@endforeach
+
 	</div>
 	<div id="navmenu"><p>{{ $recipes->links(); }}</p></div>
 @endsection
