927de44b91efd8eb4e0a5c0d5f9c50baa5cca824
bmattox1 <bmattox1@umbc.edu>
2014-04-18 18:21:13 -0400

    Added file and function headers to Recipe.php and Tag.php

--- digidiet/app/models/Recipe.php
+++ digidiet/app/models/Recipe.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Filename: Recipes.php
+ * Class invariants: a table named 'recipes' (no quotes) must exist in the database.
+ * This file must exist in the models directory for the application
+ * Author(s):
+ * Last modified: April 18, 2014
+ * Description: models a recipe of the recipes table in the database
+ */
 class Recipe extends Eloquent {
 
 	/**
@@ -9,16 +17,41 @@ class Recipe extends Eloquent {
 	 */
 	protected $table = 'recipes';
 
+	/**
+	 * Function name: user()
+	 * Description: establishes a belongsTo relationship with a particular user
+	 * preconditions: the user must exist with a valid id (author_id);
+	 * this recipe must have a valid id
+	 * postconditions: the recipe "belongs to" (is associated with) the user
+	 * @params
+	 * @return a belongsTo relationship with a particular user
+	 */
 	public function user()
 	{
 		return $this->belongsTo('User','author_id', 'id');
 	}
 
+	/**
+	 * Function name: tags()
+	 * Description: establishes a many-to-many polymorphic relationship with the Tag model
+	 * preconditions: a Tag model must exist 
+	 * postconditions: the recipe is associated with one or many tags
+	 * @params
+	 * @return a morphToMany relationship with the Tag model
+	 */
 	public function tags()
     {
         return $this->morphToMany('Tag', 'taggable');
     }
 	
+	/**
+	 * Function name: rating()
+	 * Description: establishes a one-to-many relationship with the Rating model
+	 * preconditions: a Rating model must exist
+	 * postconditions: the recipe has many ratings
+	 * @params
+	 * @return a hasMany relationship with the Rating model
+	 */
 	public function rating()
 	{
 		return $this->hasMany('Rating');
--- digidiet/app/models/Tag.php
+++ digidiet/app/models/Tag.php
@@ -1,6 +1,14 @@
 <?php
 
-class Recipe extends Eloquent {
+/**
+ * Filename: Tag.php
+ * Class invariants: a table named 'tags' (no quotes) must exist in the database.
+ * This file must exist in the models directory for the application
+ * Author(s):
+ * Last modified: April 18, 2014
+ * Description: models a tag of the tags table in the database
+ */
+class Tag extends Eloquent {
 
 	/**
 	 * The database table used by the model.
@@ -9,6 +17,14 @@ class Recipe extends Eloquent {
 	 */
 	protected $table = 'tags';
 
+	/**
+	 * Function name: recipes()
+	 * Description: establishes a many-to-many polymorphic relationship with the Recipe model
+	 * preconditions: a Recipe model must exist 
+	 * postconditions: the tag is associated with one or more recipes
+	 * @params
+	 * @return a morphedByMany relationship with the Recipe model
+	 */
 	public function recipes()
     {
         return $this->morphedByMany('Recipe', 'taggable');
