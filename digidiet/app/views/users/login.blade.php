4ef62d608a9c6e089aeab4f78decbea8f082dd86
mneary1 <mneary1@umbc.edu>
2014-04-29 01:47:15 -0400

    upon login, user alerted if they have been banned
    also changed login page slightly

--- digidiet/app/views/users/login.blade.php
+++ digidiet/app/views/users/login.blade.php
@@ -2,22 +2,37 @@
 
 @section('content')
 	<div class="span4 offset4 well">
-	{{ Form::open(array('url'=>'login', 'method'=>'post')); }}
-	<!– check for login errors flash var –>
-	@if (Session::has('login_errors'))
-		<p>Your username or password was incorrect.</p>
-	@endif
+	
+		<!– check for login errors flash var –>
+		@if (Session::has('ban'))
+			<div class="notice error">
+				<i class="icon-remove-sign icon-large"></i> 
+				<b>Unfortunately, your account has been banned. <br>Please check your email, or contact an administrator for help.</b>
+				<a href="#close" class="icon-remove"></a>
+			</div>
+		@elseif(Session::has('login_errors'))
+			<div class="notice warning">
+				<i class="icon-warning-sign icon-large"></i> 
+				Your username/password was incorrect.
+				<a href="#close" class="icon-remove"></a>
+			</div>
+		@endif
 
-	<!– username field –>
+		<div class="center">
+		{{ Form::open(array('url'=>'login', 'method'=>'post')); }}
+
+		<!– username field –>
 		<p>{{ Form::label('username', 'Username'); }}</p>
 		<p>{{ Form::text('username') }}</p>
-	
-	<!– password field –>
-	<p>{{ Form::label('password', 'Password'); }}</p>
-	<p>{{ Form::password('password'); }}</p>
-	
-	<!– submit button –>
-	<p>{{ Form::submit('Login', array('class' => 'btn-large')); }}</p>
-	{{ Form::close(); }}
+		
+		<!– password field –>
+		<p>{{ Form::label('password', 'Password'); }}</p>
+		<p>{{ Form::password('password'); }}</p>
+		
+		<!– submit button –>
+		<p>{{ Form::submit('Login', array('class' => 'btn-large')); }}</p>
+		{{ Form::close(); }}
+
+		</div>
 	</div>
 @endsection
