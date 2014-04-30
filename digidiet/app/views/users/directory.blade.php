efe1067c04a9212ac4344c442411f6afd70be961
mneary1 <mneary1@umbc.edu>
2014-04-30 04:03:47 -0400

    user directory now shows actual profile picture
    may need to look into resizing image when uploaded

--- digidiet/app/views/users/directory.blade.php
+++ digidiet/app/views/users/directory.blade.php
@@ -14,15 +14,16 @@ of the site
 -->
 @section('content')
 	<h3 style="text-align:center">User Directory</h3>
+	<div class="grid">
 	@foreach($users as $user)
 	<div class = "col_3" style="text-align:center">
 		<a href="/user/{{$user->id}}">
 		
-		<img class="caption" title="{{$user->name}}" src="http://placehold.it/150x150/{{substr(md5(rand()), 0, 6);}}
-			/ffffff.png&text={{$user->username}}" width="150" height="150" />
+		<img class="caption" title="{{$user->name}}" src="{{$user->avatar}}" width="250" height="250" />
 		</a>
 	</div>
 	@endforeach
+	</div>
 	<hr />
 	<div id="navmenu" ><h6>{{ $users->links(); }}</h6></div>
 	<hr />
