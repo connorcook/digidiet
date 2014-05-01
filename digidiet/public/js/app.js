c3546433c85ef3311143151cb81149d3a3785675
Matt <mattvlaw@gmail.com>
2014-05-01 01:38:31 -0400

    some comments

--- digidiet/public/js/app.js
+++ digidiet/public/js/app.js
@@ -1,3 +1,6 @@
+
+//create the main angular app with dependencies injected for the admin control panel.  Override {{}} notation to avoid
+//confusion with blade templating engine
 var userApp = angular.module('userApp', ['userCtrl', 'userService', 'ngAnimate'], function($interpolateProvider) {
         $interpolateProvider.startSymbol('<%');
         $interpolateProvider.endSymbol('%>');
